ed97aee2f108a48e11243943ce33b191d73d7b9f
안예진 <ahw24033@naver.com>
2021-11-11 11:31:20 +0900

    첫 사이트 레포

--- app.py
+++ app.py
@@ -1,7 +1,32 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
+
 @app.route("/")
+def hello():
+    return "<p>hello</p>"
+
+
+@app.route("/index")
 def index():
-    return "Hello, Heroku"
+    return render_template("index.html")
+
+
+@app.route("/profile")
+def profile():
+    # DB Access
+    like_foods = [
+        "두부 두루치기",
+        "김치찌개",
+        "칼국수",
+        "쌀국수",
+        "자장면",
+    ]
+
+    return render_template("profile.html", like_foods=like_foods)
+
+
+@app.route("/posts")
+def post_list():
+    return render_template("post_list.html")
